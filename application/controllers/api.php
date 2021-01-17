058a18a983035b986e01a47185034b304ec791cf
Muhammad-Junaid-Ahmad <Muhammad-Junaid-Ahmad@users.noreply.github.com>
2021-01-17 17:09:44 +0500

    Update api.php
    Changed Opening Class Brace to next line. Issue on Line#6

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -3,7 +3,8 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+class Api extends CI_Controller 
+{
 
     public function __construct() {
         parent::__construct();
